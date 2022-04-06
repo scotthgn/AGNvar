Updated spectra for correct normalisation w.r.t inclination Updated Tseed to Tseed=Tic in ADAFmod

src/model_bin/ADAFmod.py
@@ -15,8 +15,22 @@ See Kubota & Done (2018)
 """
 
 import numpy as np
-from pyNTHCOMP import donthcomp
-from DiscMods import Disc, CompDisc
+import astropy.units as u
+import astropy.constants as constants
+from .pyNTHCOMP import donthcomp
+from .DiscMods import Disc, CompDisc
+
+
+"""Usefull constants"""
+h = constants.h.value #Js - Planck constant
+G = constants.G.value * constants.M_sun.value #m^3/(M_sol^1 s^2) - Gravitational constant
+sigma = constants.sigma_sb.value #W/m^2K^4 - Stefan-Boltzmann constant
+k_B = constants.k_B.value #J/K - Boltzmann constant
+c = constants.c.value #m/s - Speed of light
+mp = constants.m_p.value #kg - Proton mass
+sigmaT = constants.sigma_T.value #m^-2 - Thomson cross section
+Ce = constants.e.value #C - elementary charge
+
 
 
 class ADAF(Disc):
@@ -90,15 +104,100 @@ class ADAF(Disc):
         self.gamma_w = gamma_w
         
         
-        #Calculating disc params
-        self._calc_Ledd() #eddington luminosity
-        self._calc_risco() #innermost stable curcular orbit
-        self._calc_efficiency() #accretion efficiency
-        self._calc_r_selfGravity() #Self gravity radius
+        #Initiating Disc - as need seed photon luminosity
+        Disc.__init__(self, r_hot, r_out, a_star, inc, mdot, M, model='AD')
+        
+        #creating warm compton model IF seed type is warm compton...
+        if self.seed_type == 'WC':
+            self.wc_mod = CompDisc(r_hot, r_out, a_star, inc, mdot, M, gamma_w, kT_w)
+            
+        
+        self._T_seed()
+        self._L_seed()
+        
+        
+    """
+    Calculating seed photon parameters - i.e luminosity and temperature
+    """
+    def _T_seed(self):
+        """
+        Calculates seed photon temperature. Assumes typical T is same as 
+        from inner edge of the disc, so T_NT(r_hot) for standard accretion
+        disc or T_NT(r_hot) * exp(y_w), where y_w is Compron y-parameter
+        for the warm comptonisation region (see Kubota & Done 2018)
+        """
+        if self.seed_type == 'AD':
+            #Ehhhm, NO. Fix this - Remeber to mask below r_tr!!
+            T_int = self.Td[0, 0] #Inner disc temperature in K
+            T_irr = self.T_rep(self.Lx)
+            T_irr = T_irr[0, 0]
+            
+            Ts = (T_int**4 + T_irr**4)**(1/4)
+            
+            kTs = k_B * Ts
+            self.kT_seed = (kTs * u.J).to(u.keV).value
+        
+        else:
+            #Using the soft-compton spectrum to estimate temperature,
+            #seeing as don't know optical depth \tau.
+            #Assuming equilibrium temperature given by inverse Compton temp.
+            wc_spec = self.wc_mod.Calc_spec(self.Lx)
+            wc_L = np.trapz(wc_spec, self.nu_grid)
+            wc_E = np.trapz(h*self.nu_grid*wc_spec, self.nu_grid)
+            
+            kT_ic = wc_E/(4 * wc_L)
+            self.kT_seed = (kT_ic * u.J).to(u.keV).value
+    
+    
+    def _L_seed(self):
+        T_int = self.Td
+        T_rep = self.T_rep(self.Lx)
+        
+        T_int = np.ma.masked_where(self.d_mask, T_int)
+        T_rep = np.ma.masked_where(self.d_mask, T_rep)
+        
+        #Flattening since no phi dependence
+        Tint_flat = T_int[0, :]
+        Trep_flat = T_rep[0, :]        
+        R_flat = self.R_grid[0, :]
+        
+        T_tot4 = (Tint_flat**4 + Trep_flat**4)
+        Ftot_r = sigma * T_tot4
+        
+        theta_0 = np.arcsin(self.Hx/R_flat)
+        cov_fac = theta_0 - 0.5 * np.sin(2*theta_0)
+        
+        self.Lseed = 2 * np.trapz(Ftot_r * (cov_fac/np.pi) * 2*np.pi * R_flat, R_flat)
+      
+        
+    
+    
+    """
+    Calculating spectrum
+    """
+    def Calc_spec(self):
+        """
+        Calculates spectrum in units W/Hz
+
+        """
+        self.Es = (self.nu_grid * u.Hz).to(u.keV, equivalencies=u.spectral()).value
+        ph_hc = donthcomp(self.Es, [self.gamma_h, self.kT_h, self.kT_seed, 1, 0])
+        ph_hc = (ph_hc * u.W/u.Hz).to(u.W/u.keV, equivalencies=u.spectral()).value  
+        
+        #normC = (self.Lseed + self.Lx)/np.trapz(ph_hc, self.nu_grid)
+        normC = self.Lx/np.trapz(ph_hc, self.nu_grid)
+        L_nu = normC * ph_hc #* np.cos(self.inc)
+                
+        
+        return L_nu
+
+            
         
         
         
 if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+    
     r_h = 25
     r_w = 100
     r_out = 400
@@ -114,5 +213,9 @@ if __name__ == '__main__':
     
     
     adaf_mod = ADAF(r_h, M, inc, mdot, a, gamma_h, kT_h, r_out)
-    print(adaf_mod.r_isco)
-        
+    Lnus = adaf_mod.Calc_spec()
+    nus = adaf_mod.nu_grid
+    
+    plt.loglog(nus, nus*Lnus)
+    plt.show()
+            

src/model_bin/DiscMods.py
@@ -23,7 +23,7 @@ from multiprocessing import Pool
 from tqdm import tqdm
 import warnings
 #import excTHCOMP as thc
-from pyNTHCOMP import donthcomp
+from .pyNTHCOMP import donthcomp
 
 #Stop all the run-time warnings (we know why they happen - doesn't affect the output!)
 warnings.filterwarnings('ignore') 
@@ -61,7 +61,7 @@ class Disc:
     
     """
     Emin = 1e-4 #keV
-    Emax = 10 #keV
+    Emax = 1e3 #keV
     hx = 10 #height of corona
     A = 0.3 #disc albedo - fixed same as in AGNSED
     numR = 400 #Nr of gridpoints in R
@@ -150,6 +150,7 @@ class Disc:
         #Creating grid over disc
         dlog_R = (np.log10(self.R_out) - np.log10(self.R_isco))/self.numR
         R_inMidBin = 10**(np.log10(self.R_isco) + dlog_R/2)
+        #R_inMidBin = 10**(np.log10(self.R_in) + dlog_R/2)
         R_outMidBin = 10**(np.log10(self.R_out) - dlog_R/2)
         
         self.R_grid, self.phi_grid = np.meshgrid(np.geomspace(
@@ -349,6 +350,11 @@ class Disc:
         
         T4 = const_fac * Rt 
         
+        #fac = 3 * (1 - (self.R_isco/self.R_grid)**(1/2))
+        #main = (G*self.M*self.Mdot)/(8*np.pi*self.R_grid**3)
+        #
+        #T4 = (1/sigma) * main * fac
+
         return T4**(1/4)
     
 
@@ -416,7 +422,7 @@ class Disc:
         #Converting from intensity to photon flux
         Fnu = np.pi * B_nu
         dLnu = 2 * 2*np.pi * Fnu * R_flat[:, np.newaxis]
-        Lnu = np.trapz(y=dLnu * np.cos(self.inc), x=R_flat, axis=0)
+        Lnu = np.trapz(y=dLnu, x=R_flat, axis=0)
         
         Ltot = np.trapz(y=Lnu, x=self.nu_grid)
         
@@ -427,7 +433,7 @@ class Disc:
     def calc_XrayPower(self):
         L_full = self.int_power(self.r_isco)
         L_tr = self.int_power(self.r_in)
-        
+
         Lxr = L_full - L_tr
         return Lxr
     
@@ -463,7 +469,7 @@ class Disc:
         
         Fnu = np.pi * B_nu
         dLs_r = 2 * np.trapz(y=Fnu, x=self.phi_grid[:, 0], axis=0)
-        Lnu = np.trapz(y=dLs_r * np.cos(self.inc) * self.R_grid[0, :], 
+        Lnu = np.trapz(y=dLs_r * np.cos(self.inc)/0.5 * self.R_grid[0, :], 
                         x=self.R_grid[0, :])
         
         return Lnu
@@ -650,10 +656,8 @@ class Disc:
             1/(np.exp((h*self.nu_grid)/(k_B * T_tot[:, np.newaxis])) - 1))
         
         F_nu = np.pi * B_nu
-        #F_nu = B_nu
         dLnu = 2 * 2*np.pi * F_nu * R_flat[:, np.newaxis]
-        Lnu = np.trapz(y=dLnu * np.cos(self.inc), x=R_flat, axis=0)
-        
+        Lnu = np.trapz(y=dLnu * np.cos(self.inc)/0.5, x=R_flat, axis=0)
         
         return Lnu
     
@@ -781,32 +785,35 @@ class CompDisc(Disc):
         Returns
         -------
         Lnu : 1D-array
-            Comptonsed disc spectrum
+            Comptonsed disc spectrum in W/Hz
 
         """
-        Es = (self.nu_grid * u.Hz).to(u.keV, equivalencies=u.spectral()).value
+        self.Es = (self.nu_grid * u.Hz).to(u.keV, equivalencies=u.spectral()).value
         Ts = self._T_seed(Lirr)
         Ts_r = np.mean(Ts, axis=0)
         
         Lloc = self._L_local(Ts)
         Lloc_r = np.trapz(Lloc, self.phi_grid[:, 0], axis=0)
-        
+
+ 
         Ts_r_kev = (Ts_r * k_B)/(Ce * 1000) #shifting to kev
         for i in range(len(self.R_grid[0, :])):
             
             if np.isscalar(Ts_r_kev[i]):
-                ph_r = donthcomp(Es, [self.gamma_c, self.kTe_c, Ts_r_kev[i], 1, 0])
-                normR = Lloc_r[i]/np.trapz(ph_r, Es)
-                ph_r = normR * ph_r
-                ph_r_nu = (ph_r * u.W/u.keV).to(u.W/u.Hz, 
+                ph_r = donthcomp(self.Es, [self.gamma_c, self.kTe_c, Ts_r_kev[i], 1, 0])
+                ph_r = (ph_r * u.W/u.keV).to(u.W/u.Hz, 
                                             equivalencies=u.spectral()).value
+                
+                normR = Lloc_r[i]/np.trapz(ph_r, self.nu_grid)
+                ph_r_nu = normR * ph_r
+                                        
                 if i == 0:
                     fs_r = ph_r_nu
                 else:
                     fs_r = np.column_stack((fs_r, ph_r_nu))
             
             else:
-                ph_r_nu = np.zeros(len(Es))
+                ph_r_nu = np.zeros(len(self.Es))
                 ph_r_nu = np.ma.masked_where(ph_r_nu==0, ph_r_nu)
                 
                 if i == 0:
@@ -814,7 +821,7 @@ class CompDisc(Disc):
                 else:
                     fs_r = np.column_stack((fs_r, ph_r_nu))
         
-        Lnu = np.trapz(2 * np.cos(self.inc) * fs_r * self.R_grid[0, :], self.R_grid[0, :])
+        Lnu = np.trapz(2 * np.cos(self.inc)/0.5 * fs_r * self.R_grid[0, :], self.R_grid[0, :])
         return Lnu
     
     